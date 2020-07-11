5ad727c31324f9867ca1f8e6453fd25dc5d993a9
shen.ke <shen.ke@whaley.cn>
2020-07-12 05:02:59 +0800

    代码提交
    Signed-off-by: shen.ke <shen.ke@whaley.cn>

--- src/Content.vue
+++ src/Content.vue
@@ -1,17 +1,26 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
     <div>
-        <up-left-right-layout></up-left-right-layout>
+        <up-left-right-layout :aside-width="220" :header-height="100">
+            <template v-slot:aside>选择</template>
+            <template v-slot:header>查看</template>
+        </up-left-right-layout>
+        <left-right-layout :aside-width="300" :header-height="100">
+            <template v-slot:aside>左右1</template>
+            <template v-slot:header>左右</template>
+        </left-right-layout>
     </div>
 </template>
 
 <script>
 
     import UpLeftRightLayout from './components/layout/UpLeftRightLayout';
+    import LeftRightLayout from "./components/layout/LeftRightLayout";
 
     export default {
         name: "Content",
         components: {
             UpLeftRightLayout,
+            LeftRightLayout,
         },
         data: function () {
             return {}
@@ -40,6 +49,4 @@
         text-align: center;
         height: 100%;
     }
-
-
 </style>
